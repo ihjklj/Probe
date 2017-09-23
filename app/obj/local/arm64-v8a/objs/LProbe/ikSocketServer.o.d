cfd38a905f7f6242c9f89268269fcaa595d652bb
ihjklj <750477889@qq.com>
2017-09-23 14:51:35 +0800

    commit

---
+++ app/obj/local/arm64-v8a/objs/LProbe/ikSocketServer.o.d
@@ -0,0 +1,182 @@
+F:/AndroidStudioProjects/Probe/app/obj/local/arm64-v8a/objs/LProbe/ikSocketServer.o: \
+  F:/AndroidStudioProjects/Probe/app/jni/ikSocketServer.cpp \
+  F:/AndroidStudioProjects/Probe/app/jni/ikSocketServer.h \
+  F:/AndroidStudioProjects/Probe/app/jni/ikUtil.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stdio.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_cprolog.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/features.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_stlport_version.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/user_config.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/compat.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/host.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/_system.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/_android.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/_gcc.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/stl_confix.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/_native_headers.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_epilog.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_config_compat_post.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\pthread.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_prolog.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\string.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stdlib.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\errno.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\vector \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/debug/_debug.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_algobase.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_cstddef.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stddef.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_cstring.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\using/cstring \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\climits \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\limits.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_cstdlib.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_cmath.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\math.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_pair.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/type_traits.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/type_manips.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_move_construct_fwk.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_iterator_base.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_ptrs_specialize.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_algobase.c \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_function_base.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_iterator.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_vector.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_alloc.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_new.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\../../gabi++/include/new \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\cstddef \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\../../gabi++/include/cstddef \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\exception \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\../../gabi++/include/exception \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl//gabi++/include/gabixx_config.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_cstdio.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_construct.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_alloc.c \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_uninitialized.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_vector.c \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_range_errors.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_bvector.h \
+  E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_relops_cont.h \
+  F:/AndroidStudioProjects/Probe/app/jni/ikThread.h
+
+F:/AndroidStudioProjects/Probe/app/jni/ikSocketServer.h:
+
+F:/AndroidStudioProjects/Probe/app/jni/ikUtil.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stdio.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_cprolog.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/features.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_stlport_version.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/user_config.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/compat.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/host.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/_system.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/_android.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/_gcc.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/stl_confix.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/config/_native_headers.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_epilog.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_config_compat_post.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\pthread.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_prolog.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\string.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stdlib.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\errno.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\vector:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/debug/_debug.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_algobase.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_cstddef.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stddef.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_cstring.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\using/cstring:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\climits:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\limits.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_cstdlib.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_cmath.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\math.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_pair.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/type_traits.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/type_manips.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_move_construct_fwk.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_iterator_base.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_ptrs_specialize.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_algobase.c:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_function_base.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_iterator.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_vector.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_alloc.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_new.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\../../gabi++/include/new:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\cstddef:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\../../gabi++/include/cstddef:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\exception:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\../../gabi++/include/exception:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl//gabi++/include/gabixx_config.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_cstdio.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_construct.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_alloc.c:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_uninitialized.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_vector.c:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_range_errors.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_bvector.h:
+
+E:/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/stlport/stlport\stl/_relops_cont.h:
+
+F:/AndroidStudioProjects/Probe/app/jni/ikThread.h:
